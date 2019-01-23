e064a8336b86ac15527c88e38b2c17c5a1bb36fd
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-01-23 23:59:52 +0900

    Add Extensions

---
+++ ThosoImage/Extensions/CompareExtension.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ThosoImage.Extensions
+{
+    public static class CompareExtension
+    {
+        /// <summary>
+        /// 値が指定範囲に収まっているか判定する
+        /// </summary>
+        /// <typeparam name="T">指定値の型</typeparam>
+        /// <param name="val">制限前の値</param>
+        /// <param name="fromValue">最小値</param>
+        /// <param name="toValue">最大値</param>
+        /// <returns>範囲外=false / 範囲内=true</returns>
+        public static bool IsRange<T>(this T val, T fromValue, T toValue) where T : IComparable
+        {
+            if (0 <= val.CompareTo(fromValue))
+            {
+                if (val.CompareTo(toValue) <= 0) return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 値を指定範囲に収めて返す
+        /// </summary>
+        /// <typeparam name="T">指定値の型</typeparam>
+        /// <param name="val">制限前の値</param>
+        /// <param name="min">最小値</param>
+        /// <param name="max">最大値</param>
+        /// <returns>制限後の値</returns>
+        public static T Limit<T>(this T val, T min, T max) where T : IComparable
+        {
+            if (0 > max.CompareTo(min)) throw new ArgumentException("Inverse(min>max)");
+            if (0 > val.CompareTo(min))
+            {
+                return min;
+            }
+            else if (val.CompareTo(max) > 0)
+            {
+                return max;
+            }
+            return val;
+        }
+
+    }
+}
---
+++ ThosoImage/Extensions/IEnumerableExtension.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThosoImage.Extensions
+{
+    public static class IEnumerableExtension
+    {
+        /// <summary>
+        /// ForEach for IEnumerable
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="sequence">Sequence</param>
+        /// <param name="action">Action</param>
+        public static void ForEach<T>(this IEnumerable<T> sequence, Action<T> action)
+        {
+            if (sequence is null) throw new ArgumentNullException(nameof(sequence));
+            if (action is null) throw new ArgumentNullException(nameof(action));
+
+            foreach (T item in sequence)
+                action(item);
+        }
+
+        /// <summary>
+        /// インデックス付きのIEnumerableを返却する
+        /// 小ネタ インデックス付き foreach  https://ufcpp.net/blog/2016/12/tipsindexedforeach/
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="source">Sequence</param>
+        /// <returns>SequenceWithIndex</returns>
+        public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            IEnumerable<(T item, int index)> impl()
+            {
+                var i = 0;
+                foreach (var item in source)
+                {
+                    yield return (item, i);
+                    ++i;
+                }
+            }
+            return impl();
+        }
+
+        /// <summary>
+        /// 番号/総数付きのIEnumerableを返却する
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="source">Sequence</param>
+        /// <returns>SequenceWithIndex&Count</returns>
+        public static IEnumerable<(T item, int index, int count)> WithRate<T>(this IEnumerable<T> source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            IEnumerable<(T item, int index, int count)> impl()
+            {
+                var count = source.Count();
+                var i = 0;
+                foreach (var item in source)
+                {
+                    yield return (item, i, count);
+                    ++i;
+                }
+            }
+            return impl();
+        }
+
+    }
+}
