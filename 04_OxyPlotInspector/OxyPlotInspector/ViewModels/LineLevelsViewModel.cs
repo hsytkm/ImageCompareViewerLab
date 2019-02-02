231e24ce9cd04b0e26e7e3feb606ec0463ad3be7
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-02-02 13:51:23 +0900

    Display LineLevels view at initialization

--- 04_OxyPlotInspector/OxyPlotInspector/ViewModels/LineLevelsViewModel.cs
+++ 04_OxyPlotInspector/OxyPlotInspector/ViewModels/LineLevelsViewModel.cs
@@ -5,7 +5,6 @@ using OxyPlotInspector.Models;
 using Prism.Mvvm;
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
-using System;
 using System.Reactive.Linq;
 
 namespace OxyPlotInspector.ViewModels
@@ -25,7 +24,6 @@ namespace OxyPlotInspector.ViewModels
         {
             OxyLineLevels = LineLevels
                 .ObserveProperty(x => x.RgbLevelLine)
-                .Where(x => x != null)
                 .Select(x => GetPlotModelSkelton(x))
                 .ToReactiveProperty();
         }
@@ -37,11 +35,14 @@ namespace OxyPlotInspector.ViewModels
             var gLine = new LineSeries { Color = OxyColors.Green };
             var bLine = new LineSeries { Color = OxyColors.Blue };
 
-            for (int i = 0; i < rgb.Length; i++)
+            if (rgb != null)
             {
-                rLine.Points.Add(new DataPoint(i, rgb[i].R));
-                gLine.Points.Add(new DataPoint(i, rgb[i].G));
-                bLine.Points.Add(new DataPoint(i, rgb[i].B));
+                for (int i = 0; i < rgb.Length; i++)
+                {
+                    rLine.Points.Add(new DataPoint(i, rgb[i].R));
+                    gLine.Points.Add(new DataPoint(i, rgb[i].G));
+                    bLine.Points.Add(new DataPoint(i, rgb[i].B));
+                }
             }
 
             pm.Series.Add(rLine);
