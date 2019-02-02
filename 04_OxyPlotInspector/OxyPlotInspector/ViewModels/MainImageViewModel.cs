7192fde056b96c8fb9a31ac4d38ebf6386a79573
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-02-03 00:03:21 +0900

    Put an arrow head on the line

--- 04_OxyPlotInspector/OxyPlotInspector/ViewModels/MainImageViewModel.cs
+++ 04_OxyPlotInspector/OxyPlotInspector/ViewModels/MainImageViewModel.cs
@@ -20,7 +20,7 @@ namespace OxyPlotInspector.ViewModels
 
         public ReadOnlyReactiveProperty<BitmapImage> ImageSource { get; }
 
-        public LinePoints LinePoints { get; } = new LinePoints();
+        public InspectLinePoints InspectLinePoints { get; } = new InspectLinePoints();
 
         #region MouseEvents
 
@@ -47,46 +47,37 @@ namespace OxyPlotInspector.ViewModels
                 .ObserveProperty(x => x.ImageSource)
                 .ToReadOnlyReactiveProperty();
 
-            ImageSource.Subscribe(x => LinePoints.SetSourceSize(x.PixelWidth, x.PixelHeight));
+            ImageSource.Subscribe(x => InspectLinePoints.SetSourceSize(x.PixelWidth, x.PixelHeight));
 
             // Viewの非表示時のクリア
             LineLevels.ObserveProperty(x => x.IsShowingView).Where(b => !b)
                 .Subscribe(_ =>
                 {
-                    LinePoints.ClearPoints();       // 画像上のLine表示
+                    InspectLinePoints.ClearPoints();       // 画像上のLine表示
                     LineLevels.ReleaseLinePoints();   // OxyPlot図(次回表示用)
                 });
 
             // マウス移動開始
             MouseDown.Where(_ => LineLevels.IsShowingView)
-                .Subscribe(p => LinePoints.SetPoint1(p.X, p.Y));
+                .Subscribe(p => InspectLinePoints.SetPoint1(p.X, p.Y));
 
             // マウス移動中
             var mouseMove = MouseDown.Merge(MouseDown.SelectMany(MouseMove.TakeUntil(MouseUp)))
                 .Where(_ => LineLevels.IsShowingView);
 
             // ViewのLine表示は常時更新
-            mouseMove.Subscribe(p => LinePoints.SetPoint2(p.X, p.Y));
+            mouseMove.Subscribe(p => InspectLinePoints.SetPoint2(p.X, p.Y));
 
             // Line画素値の取得は重いので計算を間引く
             mouseMove
                 .Throttle(TimeSpan.FromMilliseconds(500)) // 指定期間分だけ値が通過しなかったら最後の一つを流す
-                .Subscribe(_ => LineLevels.SetLinePointsRatio(LinePoints.GetPointsRatio()));
+                .Subscribe(_ => LineLevels.SetLinePointsRatio(InspectLinePoints.GetPointsRatio()));
 
         }
     }
 
     class LinePoints : BindableBase
     {
-        public int SourceWidth { get; private set; }
-        public int SourceHeight { get; private set; }
-
-        public void SetSourceSize(int width, int height)
-        {
-            SourceWidth = width;
-            SourceHeight = height;
-        }
-
         #region X1,Y1,X2,Y2
 
         private double _X1;
@@ -135,9 +126,77 @@ namespace OxyPlotInspector.ViewModels
 
         #endregion
 
-        public void ClearPoints()
+        public void ClearPoints() => X1 = X2 = Y1 = Y2 = 0.0;
+    }
+
+    class InspectLinePoints : LinePoints
+    {
+        private static readonly double ArrowRadian = 35.0 * Math.PI / 180.0;
+        private static readonly double ArrowLengthMax = 6.0;
+
+        #region LRPoints
+
+        // 矢印の左側線
+        private LinePoints _LeftWingPoints = new LinePoints();
+        public LinePoints LeftWingPoints
         {
-            X1 = X2 = Y1 = Y2 = 0;
+            get => _LeftWingPoints;
+            private set => SetProperty(ref _LeftWingPoints, value);
+        }
+
+        // 矢印の右側線
+        private LinePoints _RightWingPoints = new LinePoints();
+        public LinePoints RightWingPoints
+        {
+            get => _RightWingPoints;
+            private set => SetProperty(ref _RightWingPoints, value);
+        }
+
+        #endregion
+
+        public int SourceWidth { get; private set; }
+        public int SourceHeight { get; private set; }
+
+        public void SetSourceSize(int width, int height)
+        {
+            SourceWidth = width;
+            SourceHeight = height;
+        }
+
+        public new void SetPoint1(double x1, double y1)
+        {
+            base.SetPoint1(x1, y1);
+            LeftWingPoints.SetPoint1(x1, y1);
+            RightWingPoints.SetPoint1(x1, y1);
+        }
+
+        public new void SetPoint2(double x2, double y2)
+        {
+            base.SetPoint2(x2, y2);
+
+            // 矢印の始点は解析線の先端
+            LeftWingPoints.SetPoint1(x2, y2);
+            RightWingPoints.SetPoint1(x2, y2);
+
+            // 矢印の長さ
+            var distance = Math.Sqrt((x2 - X1) * (x2 - X1) + (y2 - Y1) * (y2 - Y1));
+            var length = Math.Min(distance, ArrowLengthMax);
+
+            // 基準線上に載りP2寄りで回転させて矢印の端にする点を求める
+            var radian = Math.Atan2(y2 - Y1, x2 - X1);
+            LeftWingPoints.SetPoint2(
+                x2 - length * Math.Cos(radian + ArrowRadian),
+                y2 - length * Math.Sin(radian + ArrowRadian));
+            RightWingPoints.SetPoint2(
+                x2 - length * Math.Cos(radian - ArrowRadian),
+                y2 - length * Math.Sin(radian - ArrowRadian));
+        }
+
+        public new void ClearPoints()
+        {
+            base.ClearPoints();
+            LeftWingPoints.ClearPoints();
+            RightWingPoints.ClearPoints();
         }
 
         // 線端の座標を割合で返す
