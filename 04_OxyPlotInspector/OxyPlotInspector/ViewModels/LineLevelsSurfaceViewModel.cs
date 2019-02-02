ccfa739f67fef41f5aaa0e47aa9e9af38fbf48da
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-02-02 20:39:29 +0900

    Add Load OxyPlot dlls lazily

---
+++ 04_OxyPlotInspector/OxyPlotInspector/ViewModels/LineLevelsSurfaceViewModel.cs
@@ -0,0 +1,18 @@
+﻿using OxyPlotInspector.Views;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace OxyPlotInspector.ViewModels
+{
+    class LineLevelsSurfaceViewModel : BindableBase
+    {
+        public DelegateCommand LazyViewLoadCommand { get; }
+
+        public LineLevelsSurfaceViewModel(IRegionManager regionManager)
+        {
+            LazyViewLoadCommand = new DelegateCommand(() =>
+                regionManager.RegisterViewWithRegion("LineLevelsRegion", typeof(LineLevels)));
+        }
+    }
+}
---
+++ 04_OxyPlotInspector/OxyPlotInspector/Views/LineLevelsSurface.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace OxyPlotInspector.Views
+{
+    /// <summary>
+    /// LineLevelsSurface.xaml の相互作用ロジック
+    /// </summary>
+    public partial class LineLevelsSurface : UserControl
+    {
+        public LineLevelsSurface()
+        {
+            InitializeComponent();
+        }
+    }
+}
