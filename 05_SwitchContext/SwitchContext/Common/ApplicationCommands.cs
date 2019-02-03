2ca9f53995b4992e95a57f2ab1efac03339f7e6e
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-02-03 21:39:34 +0900

    Add Comments

--- 05_SwitchContext/SwitchContext/Common/ApplicationCommands.cs
+++ 05_SwitchContext/SwitchContext/Common/ApplicationCommands.cs
@@ -2,13 +2,15 @@
 
 namespace SwitchContext.Common
 {
-    public interface IApplicationCommands
+    // モジュールを別プロジェクトに分けている場合、
+    // これらは更に別プロジェクトに分ける必要がある(12-UsingCompositeCommands)
+    interface IApplicationCommands
     {
         CompositeCommand SwapInnerTrackCommand { get; }
         CompositeCommand SwapOuterTrackCommand { get; }
     }
 
-    public class ApplicationCommands : IApplicationCommands
+    class ApplicationCommands : IApplicationCommands
     {
         public CompositeCommand SwapInnerTrackCommand { get; } = new CompositeCommand(true);
         public CompositeCommand SwapOuterTrackCommand { get; } = new CompositeCommand(true);
