dacecd4f73dfb9d8560827b47d1a30a5477d719d
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-02-13 20:45:55 +0900

    Change Exception Setting

--- 07_MetroWindow/MetroWindow/App.xaml.cs
+++ 07_MetroWindow/MetroWindow/App.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using MetroWindow.Views;
 using Prism.Ioc;
-using Prism.Modularity;
+using Prism.Unity;
 using System.Windows;
 
 namespace MetroWindow
@@ -8,8 +8,18 @@ namespace MetroWindow
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App
+    public partial class App : PrismApplication
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            /*
+             * 以下で例外が発生するので下記対応で回避した(本家githubのsampleでも同現象が発生してた)
+             *   例外：マネージド デバッグ アシスタント 'BindingFailure' : '表示名 'mscorlib.XmlSerializers' を伴うアセンブリを～
+             *   対応：例外設定 → Managed Debugging Assistants → BindingFailure を OFF (根本解決になってない)
+             */
+            base.OnStartup(e);
+        }
+
         protected override Window CreateShell()
         {
             return Container.Resolve<MainWindow>();
