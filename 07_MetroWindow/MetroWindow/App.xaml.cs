234c9cd0a715fc63c79ec78ba118910b94c76528
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-02-13 22:30:44 +0900

    Add Comments

--- 07_MetroWindow/MetroWindow/App.xaml.cs
+++ 07_MetroWindow/MetroWindow/App.xaml.cs
@@ -10,9 +10,11 @@ namespace MetroWindow
     /// </summary>
     public partial class App : PrismApplication
     {
-        /* 以下で例外が発生するので下記対応で回避した(本家githubのsampleでも同現象が発生してた)
+        /* 以下で例外が発生するので下記対応で回避した (根本解決になってない)
+         * 本家githubのsampleでも同現象が発生してたので…
          *   例外：マネージド デバッグ アシスタント 'BindingFailure' : '表示名 'mscorlib.XmlSerializers' を伴うアセンブリを～
-         *   対応：例外設定 → Managed Debugging Assistants → BindingFailure を OFF (根本解決になってない)
+         *   対応：例外設定 → Common Language Runtime Exceptions → System.IO.FileNotFoundException を OFF
+         *   対応：例外設定 → Managed Debugging Assistants → BindingFailure を OFF
          */
         //protected override void OnStartup(StartupEventArgs e)
         //{
