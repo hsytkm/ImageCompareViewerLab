ae0fde0e2c65dfaba1f7250bc7a5ace5fea0e4ab
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-03-14 23:03:54 +0900

    Add BlinkHighlight Project

---
+++ 08_BlinkHighlight/BlinkHilight/Models/BitmapSourceEx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace BlinkHilight.Models
+{
+    public static class BitmapSourceEx
+    {
+        /// <summary>
+        /// 引数PATHを画像として読み出す
+        /// </summary>
+        /// <param name="imagePath">ファイルパス</param>
+        /// <returns></returns>
+        public static BitmapImage ToBitmapImage(this string imagePath)
+        {
+            if (!File.Exists(imagePath)) throw new FileNotFoundException();
+
+            var bi = new BitmapImage();
+            using (var fs = File.Open(imagePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                bi.BeginInit();
+                bi.CacheOption = BitmapCacheOption.OnLoad;
+                bi.StreamSource = fs;
+                bi.EndInit();
+            }
+            bi.Freeze();
+
+            if (bi.Width == 1 && bi.Height == 1) throw new OutOfMemoryException();
+            return bi;
+        }
+    }
+}
---
+++ 08_BlinkHighlight/BlinkHilight/Models/MyImage.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Mvvm;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace BlinkHilight.Models
+{
+    class MyImage : BindableBase
+    {
+        private BitmapSource _ImageSource;
+        public BitmapSource ImageSource
+        {
+            get => _ImageSource;
+            private set => SetProperty(ref _ImageSource, value);
+        }
+
+        public MyImage()
+        {
+            var ImagePath = @"C:\data\Image1.JPG";
+
+            ImageSource = ImagePath.ToBitmapImage();
+        }
+
+        /// <summary>
+        /// 表示画像の飽和画素を点滅させる
+        /// </summary>
+        public async Task BlinkHighlightAsync()
+        {
+            var source = ImageSource;
+            var highlight = source.ToHighlighBitmapSource();
+
+            // 差分(飽和画素)がなければ終わり
+            if (source == highlight) return;
+
+            int msec = 400;
+            for (int i = 0; i < 3; i++)
+            {
+                var task0 = Task.Run(() => ImageSource = highlight);
+                var task1 = Task.Delay(msec);
+                await Task.WhenAll(task0, task1);
+
+                var task2 = Task.Run(() => ImageSource = source);
+                var task3 = Task.Delay(msec);
+                await Task.WhenAll(task2, task3);
+            }
+        }
+
+    }
+}
