74199ce13e0fc28969d288fb7f857ba05006dcad
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-03-15 23:49:55 +0900

    Add MouseWheel Skelton

---
+++ 08_ImageFunctions/ZoomThumb/ViewModels/EventConverters/MouseWheelEventToDeltaConverter.cs
@@ -0,0 +1,18 @@
+﻿using Reactive.Bindings.Interactivity;
+using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace ZoomThumb.ViewModels.EventConverters
+{
+    /// <summary>
+    /// MouseWheelイベント(dynamicは MouseWheelEventArgs 想定)
+    /// </summary>
+    public class MouseWheelEventToDeltaConverter : ReactiveConverter<dynamic, int>
+    {
+        protected override IObservable<int> OnConvert(IObservable<dynamic> source)
+        {
+            return source.Select(e => (int)e.Delta);
+        }
+    }
+}
