d6bd06e6671edb351dfc2a5244873c2cf2978cf6
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-03-18 23:55:57 +0900

    cleanup files

--- 08_ImageFunctions/ZoomThumbCodeBehind/ViewModels/ImageScrollViewerViewModel.cs
+++ 08_ImageFunctions/ZoomThumbCodeBehind/ViewModels/ImageScrollViewerViewModel.cs
@@ -84,112 +84,55 @@ namespace ZoomThumb.ViewModels
                 .Repeat()
                 .Where(_ => !ImageZoomMag.Value.IsEntire)           // ズーム中のみ流す(全画面表示中は画像移動不要)
                 .Where(_ => !temporaryZoom.Value)                   // ◆一時ズームは移動させない仕様
-                .Subscribe(v => ImageScrollOffset.Value = ShiftDraggingScrollOffset(ImageScrollOffset.Value, ImageViewSize, v));
+                .Subscribe(shift =>
+                {
+                    double clip(double value, double min, double max) => (value <= min) ? min : ((value >= max) ? max : value);
+
+                    // マウスドラッグ中の表示位置のシフト
+                    var shiftRate = new Vector(shift.X / ImageViewSize.Width, shift.Y / ImageViewSize.Height);
+
+                    ImageScrollOffset.Value = new Size(
+                        clip(ImageScrollOffset.Value.Width + shiftRate.X, 0.0, 1.0),
+                        clip(ImageScrollOffset.Value.Height + shiftRate.Y, 0.0, 1.0));
+                });
 
             #endregion
 
         }
-
-        // マウスドラッグ中の表示位置のシフト
-        private Size ShiftDraggingScrollOffset(Size offsetSize, Size imageSize, Vector shift)
-        {
-            double clip(double value, double min, double max) => (value <= min) ? min : ((value >= max) ? max : value);
-
-            var shiftRate = new Vector(shift.X / imageSize.Width, shift.Y / imageSize.Height);
-
-            return new Size(
-                clip(offsetSize.Width + shiftRate.X, 0.0, 1.0),
-                clip(offsetSize.Height + shiftRate.Y, 0.0, 1.0));
-        }
-
+        
         // クリックズームの状態を切り替える(全画面⇔等倍)
         private void SwitchClickZoomMag()
         {
             var mag = ImageZoomMag.Value.MagnificationToggle();
-
             ImageZoomMag.Value = mag;
 
             // ズーム表示への切り替えならスクロールバーを移動(ImageViewSizeを変更した後に実施する)
             if (!mag.IsEntire)
             {
+                double clip(double value, double min, double max) => (value <= min) ? min : ((value >= max) ? max : value);
+
                 var imageViewSize = ImageViewSize;
                 var scrollViewerSize = ScrollViewerSize.Value;
                 var imageSourceSize = new Size(ImageSource.Value.PixelWidth, ImageSource.Value.PixelHeight);
                 var scrollVieweMousePoint = ScrollViewerContentMouseMove.Value;
 
-                ImageScrollOffset.Value = GetClickZoomScrollOffset(
-                    mag.MagnificationRatio, imageViewSize, scrollViewerSize, scrollVieweMousePoint);
+                // 親ScrollViewerから子Imageまでのサイズ
+                var imageControlSizeOffset = new Size(
+                    Math.Max(0, scrollViewerSize.Width - imageViewSize.Width) / 2.0,
+                    Math.Max(0, scrollViewerSize.Height - imageViewSize.Height) / 2.0);
+
+                // 子Image基準のマウス位置
+                var mousePos = new Point(
+                    Math.Max(0, scrollVieweMousePoint.X - imageControlSizeOffset.Width),
+                    Math.Max(0, scrollVieweMousePoint.Y - imageControlSizeOffset.Height));
+
+                // ズーム後の中心座標の割合
+                var zoomCenterRate = new Size(
+                    clip(mousePos.X / imageViewSize.Width, 0.0, 1.0),
+                    clip(mousePos.Y / imageViewSize.Height, 0.0, 1.0));
+
+                ImageScrollOffset.Value = zoomCenterRate;
             }
-        }
-
-        // クリックズーム時のスクロールオフセットを取得
-        private static Size GetClickZoomScrollOffset(double magRatio, Size imageViewSize, Size scrollViewerSize, Point scrollVieweMousePoint)
-        {
-            double clip(double value, double min, double max) => (value <= min) ? min : ((value >= max) ? max : value);
-
-            // 親ScrollViewerから子Imageまでのサイズ
-            var imageControlSizeOffset = new Size(
-                Math.Max(0, scrollViewerSize.Width - imageViewSize.Width) / 2.0,
-                Math.Max(0, scrollViewerSize.Height - imageViewSize.Height) / 2.0);
-
-            // 子Image基準のマウス位置
-            var mousePos = new Point(
-                Math.Max(0, scrollVieweMousePoint.X - imageControlSizeOffset.Width),
-                Math.Max(0, scrollVieweMousePoint.Y - imageControlSizeOffset.Height));
-
-            // ズーム後の中心座標の割合
-            var zoomCenterRate = new Size(
-                clip(mousePos.X / imageViewSize.Width, 0.0, 1.0),
-                clip(mousePos.Y / imageViewSize.Height, 0.0, 1.0));
-            
-            return zoomCenterRate;
-        }
-    }
-
-    // 画像の倍率管理
-    public struct ImageZoomMagnification
-    {
-        private static readonly double MagRatioMin = Math.Pow(2, -5);   // 3.1%
-        private static readonly double MagRatioMax = Math.Pow(2, 5);    // 3200%
-        private static readonly double MagStep = 2.0;                   // 2倍
-
-        public bool IsEntire { get; private set; }
-        public double MagnificationRatio { get; private set; }
-
-        private ImageZoomMagnification(bool flag)
-        {
-            if (!flag) throw new ArgumentException(nameof(flag));
-            IsEntire = true;
-            MagnificationRatio = double.NaN;
-        }
-
-        private ImageZoomMagnification(double mag)
-        {
-            IsEntire = false;
-            MagnificationRatio = mag;
-        }
-
-        public static ImageZoomMagnification Entire = new ImageZoomMagnification(true);
-        private static ImageZoomMagnification MagX1 = new ImageZoomMagnification(1.0);
-
-        public ImageZoomMagnification MagnificationToggle() => IsEntire ? MagX1 : Entire;
-
-        private ImageZoomMagnification ZoomMagnification(double currentMag, double ratio)
-        {
-            // ホイールすると2の冪乗になるよう元の倍率を補正する
-            double currentMagPowerRaw = Math.Log(currentMag) / Math.Log(2);
-            double currentMagRound = Math.Pow(2, Math.Round(currentMagPowerRaw));
-
-            double newMag = currentMagRound * ratio;
-            if (newMag < MagRatioMin) newMag = MagRatioMin;
-            else if (newMag > MagRatioMax) newMag = MagRatioMax;
-            return new ImageZoomMagnification(newMag);
-        }
-
-        public ImageZoomMagnification ZoomMagnification(double currentMag, bool isZoomIn)
-        {
-            var step = isZoomIn ? MagStep : 1.0 / MagStep;
-            return ZoomMagnification(currentMag, step);
         }
 
     }
---
+++ 08_ImageFunctions/ZoomThumbCodeBehind/ViewModels/ImageZoomMagnification.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ZoomThumb.ViewModels
+{
+    /// <summary>
+    /// 画像の倍率管理
+    /// </summary>
+    public struct ImageZoomMagnification
+    {
+        private static readonly double MagRatioMin = Math.Pow(2, -5);   // 3.1%
+        private static readonly double MagRatioMax = Math.Pow(2, 5);    // 3200%
+        private static readonly double MagStep = 2.0;                   // 2倍
+
+        public bool IsEntire { get; private set; }
+        public double MagnificationRatio { get; private set; }
+
+        private ImageZoomMagnification(bool flag)
+        {
+            if (!flag) throw new ArgumentException(nameof(flag));
+            IsEntire = true;
+            MagnificationRatio = double.NaN;
+        }
+
+        private ImageZoomMagnification(double mag)
+        {
+            IsEntire = false;
+            MagnificationRatio = mag;
+        }
+
+        public static ImageZoomMagnification Entire = new ImageZoomMagnification(true);
+        private static ImageZoomMagnification MagX1 = new ImageZoomMagnification(1.0);
+
+        public ImageZoomMagnification MagnificationToggle() => IsEntire ? MagX1 : Entire;
+
+        private ImageZoomMagnification ZoomMagnification(double currentMag, double ratio)
+        {
+            // ホイールすると2の冪乗になるよう元の倍率を補正する
+            double currentMagPowerRaw = Math.Log(currentMag) / Math.Log(2);
+            double currentMagRound = Math.Pow(2, Math.Round(currentMagPowerRaw));
+
+            double newMag = currentMagRound * ratio;
+            if (newMag < MagRatioMin) newMag = MagRatioMin;
+            else if (newMag > MagRatioMax) newMag = MagRatioMax;
+            return new ImageZoomMagnification(newMag);
+        }
+
+        public ImageZoomMagnification ZoomMagnification(double currentMag, bool isZoomIn)
+        {
+            var step = isZoomIn ? MagStep : 1.0 / MagStep;
+            return ZoomMagnification(currentMag, step);
+        }
+
+    }
+}
