b1e290713715fe421ffe851ee4068aefa9276b98
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-04-21 12:59:51 +0900

    Add Graph2D Project

---
+++ 09_WPFGraphs/Graph2D/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace Graph2D.ViewModels
+{
+    class MainWindowViewModel : BindableBase
+    {
+        public static int[,] Array2D { get; } = GetArray2DRandom(9, 9, 0, 255);
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+        private static int[,] GetArray2DRandom(int xLen, int yLen, int dataMin, int dataMax)
+        {
+            var ary = new int[xLen, yLen];
+            int len0 = ary.GetLength(0);
+            int len1 = ary.GetLength(1);
+
+            var rdm = new Random();
+            foreach (var x in Enumerable.Range(0, len0 * len1)
+                .Select(_ => rdm.Next(dataMin, dataMax))
+                .Select((data, index) => (data, index)))
+            {
+                ary[x.index / len0, x.index % len1] = x.data;
+            }
+
+            ary[3, 3] = 0;
+            return ary;
+        }
+
+    }
+}
---
+++ 09_WPFGraphs/Graph2D/Views/Array2dToDataGridSourceConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Graph2D.Views
+{
+    class Array2dToDataGridSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is int[,] array)) return null;
+
+            var rows = array.GetLength(0);
+            var columns = array.GetLength(1);
+            if (rows == 0 || columns == 0) return null;
+
+            var table = new DataTable();
+
+            for (var c = 0; c < columns; c++)
+                table.Columns.Add(new DataColumn($"C{c}"));
+
+            for (var r = 0; r < rows; r++)
+            {
+                var newRow = table.NewRow();
+                for (var c = 0; c < columns; c++)
+                {
+                    newRow[c] = array[r, c];
+                }
+                table.Rows.Add(newRow);
+            }
+            return table.DefaultView;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+
+    }
+}
