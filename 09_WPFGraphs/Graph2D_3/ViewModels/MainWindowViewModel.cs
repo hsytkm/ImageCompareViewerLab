0132c3000220466f45ff0edde5f09b44d66e1e7d
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-04-25 23:16:18 +0900

    Add Reflash Command

--- 09_WPFGraphs/Graph2D_3/ViewModels/MainWindowViewModel.cs
+++ 09_WPFGraphs/Graph2D_3/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using System;
 
 namespace Graph2D.ViewModels
@@ -9,25 +10,22 @@ namespace Graph2D.ViewModels
         private static readonly int RowLength = 5;
         private static readonly int ColumnLength = 4;
 
-        public ColoredObjectRow[] Data1 { get; }
-        public ColoredObject[,] Data2 { get; }
+        private static readonly Random Random = new Random();
+
+        private ColoredObjectRow[] _Data1 = GetRandomData1();
+        public ColoredObjectRow[] Data1
+        {
+            get => _Data1;
+            private set => SetProperty(ref _Data1, value);
+        }
+
+        //public ColoredObject[,] Data2 { get; }
+
+        public DelegateCommand UpdateDataCommand { get; }
 
         public MainWindowViewModel()
         {
-            var random = new Random();
-
-            var data1 = new ColoredObjectRow[RowLength];
-            for (int r = 0; r < RowLength; r++)
-            {
-                var objects = new ColoredObject[ColumnLength];
-                for (int c = 0; c < ColumnLength; c++)
-                {
-                    objects[c] = new ColoredObject(random.Next(ValueMax), ValueMax);
-                }
-                data1[r] = new ColoredObjectRow(objects);
-            }
-            Data1 = data1;
-
+#if false
             var data2 = new ColoredObject[RowLength, ColumnLength];
             for (int c = 0; c < data2.GetLength(0); c++)
             {
@@ -38,6 +36,24 @@ namespace Graph2D.ViewModels
                 }
             }
             Data2 = data2;
+#endif
+
+            UpdateDataCommand = new DelegateCommand(() => Data1 = GetRandomData1());
+        }
+
+        private static ColoredObjectRow[] GetRandomData1()
+        {
+            var data1 = new ColoredObjectRow[RowLength];
+            for (int r = 0; r < RowLength; r++)
+            {
+                var objects = new ColoredObject[ColumnLength];
+                for (int c = 0; c < ColumnLength; c++)
+                {
+                    objects[c] = new ColoredObject(Random.Next(ValueMax), ValueMax);
+                }
+                data1[r] = new ColoredObjectRow(objects);
+            }
+            return data1;
         }
 
     }
