ec751cf36ae3da44b14b39ba4e56621a5fd086c9
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-04-25 18:58:08 +0900

    update

---
+++ 09_WPFGraphs/Graph2D_2/ViewModels/ColoredObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace Graph2D.ViewModels
+{
+    class ColoredObject
+    {
+        public object Object { get; }
+        public Color Foreground { get;}
+        public Color Background { get; }
+
+        public ColoredObject(int value, int max)
+        {
+            Object = value;
+
+            var b = (byte)Math.Min(value * 255 / max, 0xff);
+            var f = (byte)~b;
+            Background = Color.FromRgb(b, b, 0x00);
+            Foreground = Color.FromRgb(f, f, f);
+        }
+
+
+        public ColoredObject(object data, Color fore, Color back)
+        {
+            Object = data;
+            Foreground = fore;
+            Background = back;
+        }
+
+        public override string ToString() => $"{Object},{Foreground},{Background}";
+
+    }
+}
---
+++ 09_WPFGraphs/Graph2D_2/Views/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Graph2D.Views
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGridCell_Loaded(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush ToSolidColorBrush(string s) =>
+                new SolidColorBrush((Color)new ColorConverter().ConvertFrom(s));
+
+            if (!(sender is ContentControl cc)) return;
+            if (!(cc.Content is TextBlock textBlock)) return;
+
+            var data = textBlock.Text.Split(',');
+            if (data.Length != 3) return;
+
+            textBlock.Foreground = ToSolidColorBrush(data[1]);
+            textBlock.Background = ToSolidColorBrush(data[2]);
+            textBlock.Text = data[0];
+
+            textBlock.TextAlignment = TextAlignment.Right;
+        }
+
+    }
+}
