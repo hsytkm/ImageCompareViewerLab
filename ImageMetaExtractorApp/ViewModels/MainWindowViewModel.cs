10a29e1dbcae2a0a7b47ac9433d941cbf54aba13
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-07-31 16:58:56 +0900

    Add TabControl Resume

--- ImageMetaExtractorApp/ViewModels/MainWindowViewModel.cs
+++ ImageMetaExtractorApp/ViewModels/MainWindowViewModel.cs
@@ -5,13 +5,15 @@ using Prism.Mvvm;
 using Prism.Regions;
 using Reactive.Bindings;
 using System;
-using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive.Linq;
+using System.Windows.Controls;
 
 namespace ImageMetaExtractorApp.ViewModels
 {
     class MainWindowViewModel : BindableBase
     {
+        private const string MetaTabDetailsRegionName = "MetaTabDetailsRegion";
         private const string ImageSource1 = @"C:\data\Image1.JPG";
         private const string ImageSource2 = @"C:\data\Image2.JPG";
 
@@ -23,6 +25,12 @@ namespace ImageMetaExtractorApp.ViewModels
         public DelegateCommand AddTab1Command { get; }
         public DelegateCommand AddTab2Command { get; }
 
+        // 選択中のタブ要素(View)
+        public ReactiveProperty<object> TabControlSelectedItem { get; } = new ReactiveProperty<object>();
+
+        // 選択中のタブ名
+        private string _SelectedTabTitle;
+
         public MainWindowViewModel(IRegionManager regionManager)
         {
             _regionManager = regionManager;
@@ -32,6 +40,13 @@ namespace ImageMetaExtractorApp.ViewModels
 
             AddTab1Command = new DelegateCommand(AddTab1);
             AddTab2Command = new DelegateCommand(AddTab2);
+
+            // タブの選択更新時にタブ名を取得する
+            TabControlSelectedItem
+                .Select(x => (x as ContentControl)?.DataContext)
+                .Select(x => (x as MetaTabDetailViewModel)?.MetaItemGroup?.Name)
+                .Where(x => x != null)
+                .Subscribe(x => _SelectedTabTitle = x);
         }
 
         public void AddTab1() => AddTab(ImageMetas1);
@@ -39,17 +54,53 @@ namespace ImageMetaExtractorApp.ViewModels
 
         private void AddTab(ImageMetas imageMetas)
         {
-            foreach (var metaItemGroup in imageMetas.MetaItemGroups)
+            // Region追加時に選択が更新されてしまうので先にバフっとく
+            var resumeTabTitle = _SelectedTabTitle;
+            var regionName = MetaTabDetailsRegionName;
+
+            foreach (var metaItemGroup in imageMetas.MetaItemGroups.Where(x => x != null))
             {
-                if (metaItemGroup != null)
+                var parameters = new NavigationParameters
                 {
-                    var parameters = new NavigationParameters
-                    {
-                        { MetaTabDetailViewModel.MetaItemGroupKey, metaItemGroup }
-                    };
-                    _regionManager.RequestNavigate("MetaTabDetailsRegion", nameof(MetaTabDetail), parameters);
-                }
+                    { MetaTabDetailViewModel.MetaItemGroupKey, metaItemGroup }
+                };
+                _regionManager.RequestNavigate(regionName, nameof(MetaTabDetail), parameters);
             }
+            // 読み込み後の表示位置を指定
+            ActivateRegion(regionName, resumeTabTitle);
+        }
+
+        private void ActivateRegion(string regionName, string tabTitle)
+        {
+            var views = _regionManager.Regions[regionName].Views;
+            if (!views.Any()) return;
+
+            var target = views.FirstOrDefault(x => GetTabTitle(x) == tabTitle);
+            if (target != null)
+                _regionManager.Regions[regionName].Activate(target);
+            else
+                ActivateRegion(regionName, index: 0);  // ヒットしなければ先頭にする
+        }
+
+        private void ActivateRegion(string regionName, int index)
+        {
+            var views = _regionManager.Regions[regionName].Views;
+            int count = views.Count();
+            if (count == 0) return;
+
+            if (index < 0) index = 0;
+            else if (index > count) index = count - 1;
+
+            var target = views.ElementAt(index);
+            _regionManager.Regions[regionName].Activate(target);
+        }
+
+        private static string GetTabTitle(object obj)
+        {
+            if (obj is ContentControl cc)
+                if (cc.DataContext is MetaTabDetailViewModel vm)
+                    return vm.MetaItemGroup?.Name;
+            return null;
         }
 
     }
