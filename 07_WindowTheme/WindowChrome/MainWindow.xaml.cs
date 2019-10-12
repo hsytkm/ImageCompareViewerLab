1c0c94d7fac78f1db06bc6d86d5a2b0dee392236
hsytkm <t_hosoya1214@yahoo.co.jp>
2019-10-12 16:21:36 +0900

    Add SystemButtons Control

--- 07_WindowTheme/WindowChrome/MainWindow.xaml.cs
+++ 07_WindowTheme/WindowChrome/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows;
-using System.Windows.Input;
 
 namespace WindowChrome
 {
@@ -12,41 +11,6 @@ namespace WindowChrome
         {
             InitializeComponent();
         }
-
-        #region キャプションボタンのイベント
-
-        /// <summary>
-        ///        閉じる及び、終了ボタンを押した時のイベントです。
-        /// </summary>
-        private void CloseWindow(object sender, ExecutedRoutedEventArgs e)
-        {
-            // アプリを終了します。
-            SystemCommands.CloseWindow(this);
-        }
-
-        /// <summary>
-        ///        最小化ボタンを押した時のイベントです。
-        /// </summary>
-        private void MinimizeWindow(object sender, ExecutedRoutedEventArgs e)
-        {
-            // ウインドウを最小化します。
-            SystemCommands.MinimizeWindow(this);
-        }
-
-        /// <summary>
-        ///        最大化ボタンを押した時のイベントです。
-        /// </summary>
-        private void MaximizeOrRestoreWindow(object sender, ExecutedRoutedEventArgs e)
-        {
-            // ウィンドウを最大化（ もしくは元のサイズに ）します。
-            if (WindowState != WindowState.Maximized)
-                SystemCommands.MaximizeWindow(this);
-            else
-                SystemCommands.RestoreWindow(this);
-        }
-
-        #endregion
-
     }
 }
 
---
+++ 07_WindowTheme/WindowChrome/SystemButtons.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WindowChrome
+{
+    /// <summary>
+    /// SystemButtons.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SystemButtons : UserControl
+    {
+        private Window Window
+        {
+            get
+            {
+                if (_window is null)
+                    _window = Window.GetWindow(this);
+                return _window;
+            }
+        }
+        private Window _window;
+
+        public SystemButtons()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// アプリを終了します
+        /// </summary>
+        private void CloseWindow(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(Window);
+        }
+
+        /// <summary>
+        /// ウインドウを最小化します
+        /// </summary>
+        private void MinimizeWindow(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(Window);
+        }
+
+        /// <summary>
+        /// ウィンドウを最大化（ もしくは元のサイズに ）します
+        /// </summary>
+        private void MaximizeOrRestoreWindow(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (Window.WindowState != WindowState.Maximized)
+                SystemCommands.MaximizeWindow(Window);
+            else
+                SystemCommands.RestoreWindow(Window);
+        }
+
+    }
+}
